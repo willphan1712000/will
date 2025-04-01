c445bb9bbd123ea43299393e3de16b96a8c23e55
willphan <phanthanhnha1712000@gmail.com>
2025-04-01 08:46:39 -0400

    Hide login button due to no need to sign in as users visit the page at the moment

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { useEffect } from 'react'
 import { BsAirplane } from 'react-icons/bs'
 import { FaHome } from 'react-icons/fa'
 import { ImBlog } from 'react-icons/im'
 import { IoLibrary } from 'react-icons/io5'
+import { VscSignIn } from 'react-icons/vsc'
 import Logo from './Logo'
 
 const href: Record<string, string> = {
@@ -23,6 +25,7 @@ const icon: Record<string, React.ReactNode> = {
 }
 
 const Navbar = () => {
+  const { status, data: session } = useSession()
   useEffect(() => {
     const navBar = document.getElementById("nav-bar") as HTMLElement
 
@@ -43,7 +46,7 @@ const Navbar = () => {
   }, [])
   
   return (
-    <div id="nav-bar" className='fixed top-0 md:relative flex flex-row items-center justify-center p-[5px] z-[3] bg-white w-full h-20 transition-all'>
+    <div id="nav-bar" className='fixed top-0 md:relative flex flex-col md:flex-row items-center justify-center p-[5px] z-[3] bg-white w-full h-20 transition-all'>
         <div className="nav-bar__logo mr-[40px] hidden md:flex">
             <Logo />
         </div>
@@ -51,6 +54,15 @@ const Navbar = () => {
             <ul>
               {Object.keys(href).map(key => <li key={key}><Link href={href[key]}><span className='py-[10px] px-[10px] md:px-[20px] rounded-[10px] hover:bg-[#f0f0f0] text-[1rem] md:text-[1.25rem] flex flex-row justify-center items-center gap-2'>{icon[key]}{key}</span></Link></li>)}
             </ul>
+        </div>
+        <div className='flex flex-row justify-center items-center gap-2'>
+          { status === 'authenticated' && (
+            <>
+              <img src={ session.user?.image! } alt='' width="25" height="25" className='rounded-full'/>
+              <p>{session.user?.name}</p>
+              <Link className='cursor-pointer' href={'/api/auth/signout'}><VscSignIn size={20} /></Link>
+            </>
+        )}
         </div>
     </div>
   )
